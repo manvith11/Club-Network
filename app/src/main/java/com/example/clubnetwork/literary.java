22d67c94310a33729f75a08daa1781486301b28e
manvith11 <manvithk27@gmail.com>
2024-02-04 17:09:15 +0530

    finished project

---
+++ app/src/main/java/com/example/clubnetwork/literary.java
@@ -0,0 +1,15 @@
+package com.example.clubnetwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class literary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_literary);
+
+    }
+}
